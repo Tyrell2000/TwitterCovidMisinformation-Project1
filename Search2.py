b67f400ca92df19c058ee281f28e19d2f5477a10
Tyrell2000 <44913702+Tyrell2000@users.noreply.github.com>
2021-07-08 13:58:18 -0400

    csv to txt

--- Search2.py
+++ Search2.py
@@ -7,8 +7,8 @@ import datetime
 
 seedURL = 'https://thehill.com/policy/healthcare/public-global-health/561627-pfizer-vaccine-less-effective-against-delta-variant'
 date = datetime.date.today().strftime('%Y-%m-%d')
-# csvName = seedURL + '_' + str(date)
-csvName = 'seedsset' + '_' + str(date)
+# txtName = seedURL + '_' + str(date)
+txtName = 'seedsset' + '_' + str(date)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -17,43 +17,43 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 baseLink = urlparse(url).netloc
 
-csv = open(csvName + '.csv', "w+", encoding="utf-8")
-csv.close()
+txt = open(txtName + '.txt', "w+", encoding="utf-8")
+txt.close()
 
-# csvContent = list(open(csvName + '.csv', 'r'))
-# print(len(list(open(csvName + '.csv', 'r'))))
-# print(list(csvContent))
+# txtContent = list(open(txtName + '.txt', 'r'))
+# print(len(list(open(txtName + '.txt', 'r'))))
+# print(list(txtContent))
 
 urls = []
 
 counter = 0
 for link in soup.find_all('a'):
-    csv = open(csvName + '.csv', 'a', encoding="utf-8")
+    txt = open(txtName + '.txt', 'a', encoding="utf-8")
     getLink = link.get('href')
 
     if getLink[0] == '/':
         getLink = 'http://' + baseLink + getLink
 
-    if getLink + '\n' not in list(open(csvName + '.csv', 'r')):
-        # print(list(open(csvName + '.csv', 'r')))
+    if getLink + '\n' not in list(open(txtName + '.txt', 'r')):
+        # print(list(open(txtName + '.txt', 'r')))
         if link.get('href')[0] == '/' or 'http://' in link.get('href'):
             ##print(getLink)
             if link.get('href')[0] == '/':
-                csv.write('http://' + urlparse(url).netloc + link.get('href'))
+                txt.write('http://' + urlparse(url).netloc + link.get('href'))
 
             elif 'http://' in link.get('href') and link.get('href') not in urls:
                 urls.append(link.get('href'))
-                csv.write(link.get('href'))
+                txt.write(link.get('href'))
 
             counter = counter + 1
-            csv.write('\n')
-            csv.close()
+            txt.write('\n')
+            txt.close()
     if counter == 5:
         break
 
-for URL in open(csvName + '.csv', 'r'):
+for URL in open(txtName + '.txt', 'r'):
     # print('url: '+ URL)
-    if len(list(open(csvName + '.csv', 'r'))) >= 1000:
+    if len(list(open(txtName + '.txt', 'r'))) >= 1000:
         break
     url = URL
     page = requests.get(url.strip())
@@ -64,26 +64,26 @@ for URL in open(csvName + '.csv', 'r'):
     counter = 0
     for link in soup.find_all('a'):
         if len(link.get('href')) > 0:
-            csv = open(csvName + '.csv', 'a', encoding="utf-8")
+            txt = open(txtName + '.txt', 'a', encoding="utf-8")
             getLink = link.get('href')
 
             if getLink[0] == '/':
                 getLink = 'http://' + baseLink + getLink
-            if getLink + '\n' not in list(open(csvName + '.csv', 'r')):
-                # print(list(open(csvName + '.csv', 'r')))
+            if getLink + '\n' not in list(open(txtName + '.txt', 'r')):
+                # print(list(open(txtName + '.txt', 'r')))
                 if link.get('href')[0] == '/' or 'http://' in link.get('href'):
                     print(len(urls), ": " + getLink)
                     if link.get('href')[0] == '/' and getLink not in urls:
                         urls.append(getLink)
                         # print('if / is in link:' + urlparse(url).netloc + link.get('href'))
-                        csv.write('http://' + urlparse(url).netloc + link.get('href'))
+                        txt.write('http://' + urlparse(url).netloc + link.get('href'))
 
                     elif 'http://' in link.get('href'):
                         # print('if / is not in link:' + urlparse(url).netloc + link.get('href'))
-                        csv.write(link.get('href'))
+                        txt.write(link.get('href'))
 
                     counter = counter + 1
-                    csv.write('\n')
-                    csv.close()
+                    txt.write('\n')
+                    txt.close()
             if counter == 5:
                 break
