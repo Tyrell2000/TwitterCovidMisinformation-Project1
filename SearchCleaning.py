1d63672299695c560d2c8e3dc2c0d6b1f20c016a
Tyrell2000 <44913702+Tyrell2000@users.noreply.github.com>
2021-07-08 10:20:05 -0400

    Seed Cleaning Usage Update
    Added more usage instructions and options for seed cleaning, Added compatibility for user to add urls from scraping(grab new seed from current page and add to seed set then iterate through the new seeds)(untested).

--- SearchCleaning.py
+++ SearchCleaning.py
@@ -18,11 +18,12 @@ headers = {
 url1 = 'https://thehill.com/policy/healthcare/public-global-health/561627-pfizer-vaccine-less-effective-against-delta-variant'
 url2 = 'https://www.cnn.com/2021/07/06/politics/republican-candidates-trump-election-lie/index.html'
 url3 = 'https://www.cdc.gov/'
+url4 = 'https://www.foxnews.com/us/rescuers-at-florida-condo-collapse-told-they-can-go-home-but-they-refuse'
 urls = [url1, url2, url3]
 
 # change this, depending on if you have a file with urls(a seed set) already. True to use an existing file, False to
 # create one
-alreadyHaveSeedSet = False
+alreadyHaveSeedSet = True
 
 # open the file with the seed set and clear it. set alreadyHaveSeedSet to False if you want to load a seed set
 # from a file
@@ -38,10 +39,13 @@ if alreadyHaveSeedSet:
     for url in seedSet.readlines():
         urls.append(url)
 
+seedSetLength = len(urls)
+currentSeedNumber = 0
+notAtLastSeed = True
 # go through every url in the list(code equivalent of seed set)
-for urlNumber in range(len(urls)):
+while notAtLastSeed:
     # open web page
-    page = requests.get(urls[urlNumber], headers=headers)
+    page = requests.get(urls[currentSeedNumber], headers=headers)
 
     # set soup equal to the webpage html contents
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -53,7 +57,7 @@ for urlNumber in range(len(urls)):
     visible_texts = filter(tag_visible, texts)
 
     # create/open file for this seed, named after number in the list of seeds starting from 0
-    currentSeed = open(str(urlNumber) + ".txt", "w", encoding="utf-8")
+    currentSeed = open(str(currentSeedNumber) + ".txt", "w", encoding="utf-8")
 
     # cleaning the text that's added to this seed's file
     for lineOfText in visible_texts:
@@ -64,16 +68,40 @@ for urlNumber in range(len(urls)):
     # close the file associated with this seed. we are done adding text to it
     currentSeed.close()
 
+    # will be entered if you have specified that you are creating a seed set earlier in the code
     if not alreadyHaveSeedSet:
-        # open the seed list text file
+        # add seeds you want to put in the seed set to this list and they will be added later for processing
+        nextSeeds = []
+
+        '''for i in range(2):
+            nextSeeds.append("urlhere")'''
+
+        # open the seed set text file
         seedSet = open("seedSet.txt", "a", encoding="utf-8")
 
         # write the current url to the seed list file
-        seedSet.write(urls[urlNumber])
+        seedSet.write(urls[currentSeedNumber])
 
-        # add a new line to separate the current url and the next url
-        if urlNumber != len(urls) - 1:
+        # for adding the next seed(s) to be added to the set, not implemented atm
+        for seed in nextSeeds:
+            urls.append(seed)
+
+        # clear the list, since the new seeds have now been added to the primary seed set
+        nextSeeds = []
+
+        # add a new line to separate the current url and the next url, if not the last seed
+        if currentSeedNumber != len(urls) - 1:
             seedSet.write("\n")
 
-        # close the file associated with the seed set. we are done adding urls to it
+        # close the file associated with the seed set. we are done adding urls to it atm
         seedSet.close()
+
+    # update the stored length of the seed set
+    seedSetLength = len(urls)
+
+    # increment the current seed in the set by 1
+    currentSeedNumber += 1
+
+    # if you are at the last seed in the set, stop the loop
+    if currentSeedNumber == seedSetLength:
+        notAtLastSeed = False
