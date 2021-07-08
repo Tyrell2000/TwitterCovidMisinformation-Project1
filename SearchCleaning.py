b0ee52f9a5450d1966b51ac4e64d9704369ae283
Tyrell2000 <44913702+Tyrell2000@users.noreply.github.com>
2021-07-08 09:45:55 -0400

    Seed Cleaning Usage Additions
    Added usage instructions to the seedCleaning.txt file and added a few new usage options. New features will make seed set creation easier, when adding code to grab seeds.

--- SearchCleaning.py
+++ SearchCleaning.py
@@ -20,25 +20,60 @@ url2 = 'https://www.cnn.com/2021/07/06/politics/republican-candidates-trump-elec
 url3 = 'https://www.cdc.gov/'
 urls = [url1, url2, url3]
 
-seedSet = open("seedSet.txt", "a", encoding="utf-8")
-seedSet.truncate(0)
+# change this, depending on if you have a file with urls(a seed set) already. True to use an existing file, False to
+# create one
+alreadyHaveSeedSet = False
 
+# open the file with the seed set and clear it. set alreadyHaveSeedSet to False if you want to load a seed set
+# from a file
+if not alreadyHaveSeedSet:
+    seedSet = open("seedSet.txt", "a", encoding="utf-8")
+    seedSet.truncate(0)
+
+# use this loop if you want to load a seed set from a file. just change the file name and set alreadyHaveSeedSet to
+# True to activate this loop
+if alreadyHaveSeedSet:
+    urls = []
+    seedSet = open("seedSet.txt", "r", encoding="utf-8")
+    for url in seedSet.readlines():
+        urls.append(url)
+
+# go through every url in the list(code equivalent of seed set)
 for urlNumber in range(len(urls)):
+    # open web page
     page = requests.get(urls[urlNumber], headers=headers)
+
+    # set soup equal to the webpage html contents
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    # get all text from the webpage
     texts = soup.findAll(text=True)
+
+    # filter out invisible text in the html
     visible_texts = filter(tag_visible, texts)
 
+    # create/open file for this seed, named after number in the list of seeds starting from 0
     currentSeed = open(str(urlNumber) + ".txt", "w", encoding="utf-8")
 
+    # cleaning the text that's added to this seed's file
     for lineOfText in visible_texts:
         if lineOfText != "\n" and lineOfText != " ":
             currentSeed.write(re.sub('[\t\n]', "", lineOfText))
             currentSeed.write("\n")
 
+    # close the file associated with this seed. we are done adding text to it
     currentSeed.close()
 
-    seedSet = open("seedSet.txt", "a", encoding="utf-8")
-    seedSet.write(urls[urlNumber])
-    seedSet.write("\n")
-    seedSet.close()
+    if not alreadyHaveSeedSet:
+        # open the seed list text file
+        seedSet = open("seedSet.txt", "a", encoding="utf-8")
+
+        # write the current url to the seed list file
+        seedSet.write(urls[urlNumber])
+
+        # add a new line to separate the current url and the next url
+        if urlNumber != len(urls) - 1:
+            seedSet.write("\n")
+
+        # close the file associated with the seed set. we are done adding urls to it
+        seedSet.close()
